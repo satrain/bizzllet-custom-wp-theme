821f0dbd78ce9bf81746888cbf3a5d08749a5360
Mladen Trickovic <48336740+satrain@users.noreply.github.com>
2020-10-03 03:10:29 +0200

    Uploaded new files

---
+++ bizzllet/footer.php
@@ -0,0 +1,56 @@
+<footer>
+        <div class="newsletter-section">
+            <div class="newsletter-bgr-left"></div>
+            <div class="newsletter-bgr-right"></div>
+
+            <h2>Business is easier with crypto.</h2>
+            <h3>Join the herd today and experience the full power of crypto!</h3>
+            <form>
+                <input type="email" class="email-input" placeholder="Your email address">
+                <input type="submit" class="submit-input" value="Sign Up For Early Access">
+            </form>
+        </div>
+
+        <div class="footer-section">
+            <div class="footer-about-section">
+                <div class="footer-bizzllet-logo"></div>
+                <p>
+                    Want to learn more about crypto?<br>
+                    Check our <a href="#">blog</a>
+                </p>
+            </div>
+            <div class="footer-socialmedias">
+                <div class="footer-ellipse">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/images/linkedin-icon.png" alt="">
+                </div>
+                <div class="footer-ellipse">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/images/twitter-icon.png" alt="">
+                </div>
+                <div class="footer-ellipse">
+                    <img src="<?php bloginfo('template_directory'); ?>/assets/images/cb-icon.png" alt="">
+                </div>
+            </div>
+            <div class="footer-nav-shortcuts">
+                <ul class="first-ul">
+                    <li><a href="#">FAQ</a></li>
+                    <li><a href="#">Privacy</a></li>
+                    <li><a href="#">Policies</a></li>
+                </ul>
+                <ul class="second-ul">
+                    <li><a href="#">Problem we're solving</a></li>
+                    <li><a href="#">Product feature set</a></li>
+                    <li><a href="#">Our team</a></li>
+                </ul>
+            </div>
+        </div>
+    </footer>
+
+    <div class="footer-rights-section">
+        <p>All rights reserved 2020 Bizzllet</p>
+    </div>
+
+    <?php wp_footer(); ?>
+
+</body>
+
+</html>
